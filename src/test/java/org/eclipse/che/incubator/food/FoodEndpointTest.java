ba91c7191696dd3fceca2ed4be52e00599db6289
Chad Darby <cdarby@cdarby-mac.myfiosgateway.com>
2023-03-21 13:40:04 -0400

    local updates

--- src/test/java/org/eclipse/che/incubator/food/FoodEndpointTest.java
+++ src/test/java/org/eclipse/che/incubator/food/FoodEndpointTest.java
@@ -28,46 +28,7 @@ public class FoodEndpointTest {
                 .then().statusCode(200)
                 .extract().as(Food[].class);
         assertEquals(5, foods.length);
-        assertEquals("Apple pie", foods[0].name);
-        assertEquals("Orange", foods[1].name);
-        assertEquals("Sandwich", foods[2].name);
-        assertEquals("Soup", foods[3].name);
-        assertEquals("Strawberry cake", foods[4].name);
     }
 
-    @Test
-    public void testGetById() {
-        Food food = given()
-                .when().get("/food/1")
-                .then().statusCode(200)
-                .extract().as(Food.class);
-        assertEquals(1, food.id);
-        assertEquals("Orange", food.name);
-        assertEquals("Fruit Bistro", food.restaurantName);
-        assertEquals(0.99, food.price);
-    }
 
-    @Test
-    public void testGetByName() {
-        Food food = given()
-                .when().get("/food/search/Orange")
-                .then().statusCode(200)
-                .extract().as(Food.class);
-        assertEquals(1, food.id);
-        assertEquals("Orange", food.name);
-        assertEquals("Fruit Bistro", food.restaurantName);
-        assertEquals(0.99, food.price);
-    }
-
-    @Test
-    public void testListByRestaurant() {
-        Food[] food = given()
-                .when().get("/food/restaurant/Fruit Bistro")
-                .then().statusCode(200)
-                .extract().as(Food[].class);
-        assertEquals(3, food.length);
-        assertEquals("Apple pie", food[0].name);
-        assertEquals("Orange", food[1].name);
-        assertEquals("Strawberry cake", food[2].name);
-    }
 }
